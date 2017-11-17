f5b42acb5287a2f435b9a6923f983675f7169b43
steven athouel <sathouel@gmail.com>
2017-11-17 11:19:39 +0100

    include dir added, instrument.hpp & server.hpp too

---
+++ include/instrument.hpp
@@ -0,0 +1,12 @@
+//
+// Created by steven athouel on 16/11/2017.
+//
+
+#ifndef FANFARESIMULATOR_INSTRUMENT_HPP
+#define FANFARESIMULATOR_INSTRUMENT_HPP
+
+enum Instrument{
+    // instruments here !
+};
+
+#endif //FANFARESIMULATOR_INSTRUMENT_HPP
---
+++ include/server.hpp
@@ -0,0 +1,58 @@
+//
+// Created by steven athouel on 16/11/2017.
+//
+
+#ifndef FANFARESIMULATOR_SERVER_HPP
+#define FANFARESIMULATOR_SERVER_HPP
+
+#include <iostream>
+#include <queue>
+#include <map>
+#include "instrument.hpp"
+
+typedef std::basic_string<char> string;
+
+// Waiting for implementation
+class Obj{};
+
+typedef Obj ServerSocket;
+typedef Obj Socket;
+typedef Obj Synthesizer;
+
+//
+
+class Server {
+private:
+    ServerSocket serverSocket;
+    std::queue incomingNotes;
+    Synthesizer synthesizer;
+
+    std::map< string, std::pair< Socket, bool > > clients;  // key : username, vale : (socket, isReady)
+    std::map< string, Instrument > usrToInstrument;         // key : username, value : Instrument
+    std::map< Instrument , bool > instrumentMap;          // key : Instrument, value : available
+
+public:
+
+    void broadcastStart() const ;
+
+    void updateInstrumentMap(Instrument i) ;
+
+    bool addClient(string username) ;
+
+    bool addClient(string username, Instrument i) ;
+
+    void sendPartition(string username) ;
+
+    std::map< Instrument , bool > getInstrumentMap() const ;
+
+
+
+
+
+
+};
+
+
+
+
+#endif //FANFARESIMULATOR_SERVER_HPP
