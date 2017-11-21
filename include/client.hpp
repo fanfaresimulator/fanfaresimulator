afcb7a34945d9d8314189e45b63340899a4d5276
Vincent <Vincent.ragel@polytechnique.edu>
2017-11-21 14:52:09 +0100

    Client Interface updated (client.hpp)

--- include/client.hpp
+++ include/client.hpp
@@ -5,8 +5,53 @@
 #ifndef FANFARESIMULATOR_CLIENT_HPP
 #define FANFARESIMULATOR_CLIENT_HPP
 
+#include <iostream>
+#include "instrument.hpp"
+#include "partition.hpp"
+// #include "keyboard.hpp"  !! Waiting for Keyboard
+
+typedef std::basic_string<char> string;
+
+// Waiting for implementation
+class Obj{};
+
+typedef Obj Socket;
+typedef Obj Keyboard;
+typedef Obj Note;
+
+//
 
 class client {
+
+private :
+    Socket socket;              // !!! use the class Socket => Remark : waiting for the implementation
+    string username;            // use nothing ?
+    Instrument instrument;      // use the class Instrument
+    Partition partition;        // Use the class Partition
+    Keyboard keyboard;          // !!! Waiting for the class Keyboard
+
+public:
+
+    client();
+
+    // Other constructer can be created
+
+    ~client();
+
+    void sendNote(Note note);
+
+    void sendReady();
+
+    void sendAddClient(string username);
+
+    void sendAddInstrumentToClient(string username, Instrument instrument);
+
+    void requestPartition(Partition partition);
+
+    void loadPartition(Partition partition);
+
+    Instrument setInstrument();
+
 
 };
 
