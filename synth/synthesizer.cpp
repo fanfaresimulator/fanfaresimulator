80fbf004cdc1e51cabdadd01c950212e7d34e542
Pablo Le Hénaff <pablo.le-henaff@polytechnique.edu>
2017-11-21 16:19:09 +0100

    Rien

--- synth/synthesizer.cpp
+++ synth/synthesizer.cpp
@@ -1,7 +1,7 @@
 #include "synthesizer.hpp"
 
 #include <stdio.h>
-
+#include <unistd.h>
 
 #include "fluidsynth.h"
 #include "fluidsynth/settings.h"
@@ -11,15 +11,34 @@ void testSynth () {
 	printf("Hello world from the synth !");
 	
 	
-	
-	fluid_settings_t* settings;
+    fluid_settings_t* settings;
     fluid_synth_t* synth;
+    fluid_audio_driver_t* adriver;
     settings = new_fluid_settings();
+    /* Set the synthesizer settings, if necessary */
     synth = new_fluid_synth(settings);
+    fluid_settings_setstr(settings, "audio.driver", "jack"); // à changer
     
     
+    adriver = new_fluid_audio_driver(settings, synth);
+    
+    
+    fluid_synth_sfload(synth, "sf.sf2", 1);
+    
+    int key;
+	for (int i = 0; i < 12; i++) {
+		/* Generate a random key */
+		key = 60;
+		/* Play a note */
+		fluid_synth_noteon(synth, 0, key, 80);
+		/* Sleep for 1 second */
+		sleep(1);
+		/* Stop the note */
+		fluid_synth_noteoff(synth, 0, key);
+	}
     
     /* Do useful things here */
+    delete_fluid_audio_driver(adriver);
     delete_fluid_synth(synth);
     delete_fluid_settings(settings);
 	
