8932966bc8497526e91096332a3ed9037279399d
Vincent <Vincent.ragel@polytechnique.edu>
2017-11-21 13:45:12 +0100

    Client.hpp added CMakeLists.txt modified in order to add client.hpp in the executable.

---
+++ include/client.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Ragel on 21/11/2017.
+//
+
+#ifndef FANFARESIMULATOR_CLIENT_HPP
+#define FANFARESIMULATOR_CLIENT_HPP
+
+
+class client {
+
+};
+
+
+#endif //FANFARESIMULATOR_CLIENT_HPP
