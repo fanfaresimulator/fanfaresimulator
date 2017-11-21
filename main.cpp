703317e77e78ae2971c899b1723e6219e8efb724
LucieGillet <33422201+LucieGillet@users.noreply.github.com>
2017-11-21 17:26:26 +0100

    add first_welcome_window

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
-#include <QApplication>
+//#include <QApplication>
+#include <QtWidgets/QApplication>
 #include "gui/testwindow.hpp"
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QGraphicsView>
+#include <qinputdialog>
 
 int main(int argc, char *argv[])
 {
@@ -9,5 +13,28 @@ int main(int argc, char *argv[])
 
 	window.show();
 
+	bool dialogResult;
+	QInputDialog *renameDialog = new QInputDialog();
+	renameDialog->setTextValue("Test"); // has no effect
+	QString result = renameDialog->getText(0, "Rename Label", "New name:", QLineEdit::Normal,
+		"", &dialogResult);
+
+
+
+
 	return app.exec();
 }
+
+/*int main(int argc, char *argv[])
+{
+QApplication app(argc, argv);
+
+MyScene scene;
+scene.setSceneRect(-150, -150, 300, 300);
+
+QGraphicsView view(&scene);
+view.setWindowTitle("Ma première scène");
+view.show();
+return app.exec();
+}*/
+
