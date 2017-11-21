2af535d134b2a0a809665eb375affa8c7d89be63
FooB4r <bambambambooboo@gmail.com>
2017-11-21 13:59:21 +0100

    Better string & format refactoring

--- include/server.hpp
+++ include/server.hpp
@@ -1,61 +1,61 @@
-//
-// Created by steven athouel on 16/11/2017.
-//
 
 #ifndef FANFARESIMULATOR_SERVER_HPP
 #define FANFARESIMULATOR_SERVER_HPP
 
-#include <iostream>
-#include <queue>
-#include <map>
 #include "instrument.hpp"
+#include <iostream>
+#include <map>
+#include <queue>
+#include <string>
 
-typedef std::basic_string<char> string;
+typedef enum {
+  HELLO,
+  NOTE,
+  CHOICE_LOBBY,
+  CHOICE_INSTRUMENT,
+  LOBBIES,
+  INSTRUMENTS,
+  PARTITION
+} protocol_sig;
 
 // Waiting for implementation
-class Obj{};
+class Obj {};
 
 typedef Obj ServerSocket;
 typedef Obj Socket;
 typedef Obj Synthesizer;
+typedef Obj Note;
 
 //
 
 class Server {
 private:
-    ServerSocket serverSocket;
-    std::queue incomingNotes;
-    Synthesizer synthesizer;
+  ServerSocket serverSocket;
+  std::queue<Note> incomingNotes;
+  Synthesizer synthesizer;
 
-    std::map< string, std::pair< Socket, bool > > clients;  // key : username, vale : (socket, isReady)
-    std::map< string, Instrument > usrToInstrument;         // key : username, value : Instrument
-    std::map< Instrument , bool > instrumentMap;          // key : Instrument, value : available
+  std::map<std::string, std::pair<Socket, bool>> clients; // key : username, vale : (socket, isReady)
+  std::map<std::string, Instrument> usrToInstrument; // key : username, value : Instrument
+  std::map<Instrument, bool> instrumentMap; // key : Instrument, value : available
 
 public:
+  Server();
 
-    Server();
+  Server(ServerSocket serverSocket);
 
-    Server(ServerSocket serverSocket);
+  ~Server();
 
-    ~Server();
+  void broadcastStart() const;
 
-    void broadcastStart() const ;
+  void updateInstrumentMap(Instrument i);
 
-    void updateInstrumentMap(Instrument i) ;
+  bool addClient(std::string username);
 
-    bool addClient(string username) ;
+  bool addClient(std::string username, Instrument i);
 
-    bool addClient(string username, Instrument i) ;
+  void sendPartition(std::string username);
 
-    void sendPartition(string username) ;
-
-    std::map< Instrument , bool > getInstrumentMap() const ;
-
-
-
+  std::map<Instrument, bool> getInstrumentMap() const;
 };
 
-
-
-
-#endif //FANFARESIMULATOR_SERVER_HPP
+#endif // FANFARESIMULATOR_SERVER_HPP
