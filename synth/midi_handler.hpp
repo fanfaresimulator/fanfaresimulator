a9ae5cffb3b96def802e38fbd8502708fc9ff3c4
Romain Loiseau <romain.loiseau@polytechnique.edu>
2017-11-22 15:00:28 +0100

    midi_handler first step

--- synth/midi_handler.hpp
+++ synth/midi_handler.hpp
@@ -1,6 +1,6 @@
-#include "libmidifile/MidiFile.h"
-#include "libmidifile/Options.h"
 #include <iostream>
 #include <iomanip>
+#include "libmidifile/MidiFile.h"
+#include "libmidifile/Options.h"
 
 void midi_handler();
