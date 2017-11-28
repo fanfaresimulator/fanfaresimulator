0f115955540a8d4dad11b17561af1bf558335f74
anth2o <33425718+anth2o@users.noreply.github.com>
2017-11-28 17:56:41 +0100

    Update game_window.cpp

--- gui/game_window.cpp
+++ gui/game_window.cpp
@@ -6,29 +6,6 @@ GameWindow::GameWindow() : QWidget()
     set_size(1000, 1000);
     set_number_of_lines(4);
     create_lines();
-
-
-    /*
-    setFixedSize(get_height(), get_width());
-
-    m_slider = new QSlider(Qt::Horizontal, this);
-    m_slider->setGeometry(10, 60, 150, 20);
-
-    m_bar = new QProgressBar(this);
-    m_bar->setGeometry(10, 20, 150, 20);
-    m_bar->setOrientation(Qt::Horizontal);
-
-    QObject::connect(m_slider, SIGNAL(valueChanged(int)), m_bar, SLOT(setValue(int))) ;
-
-    line = new MusicLine(this);
-    line->setGeometry(0,200,1000,1000);
-    line->show();
-
-    note1 = new Note(this);
-    note1->setGeometry(0,200,1000,1000);
-    note1->show();7
-
-     */
 }
 
 GameWindow::GameWindow(int width, int height) : QWidget() {
