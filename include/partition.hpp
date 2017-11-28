5f74ab6285d7a1d9673b4925297d2a46a8189b6c
GiulioGargantini <33402451+GiulioGargantini@users.noreply.github.com>
2017-11-28 16:38:01 +0100

    partition

--- include/partition.hpp
+++ include/partition.hpp
@@ -10,10 +10,12 @@ class Partition {
 private:
 	std::list <Note> listOfNotes;
 
+
+
 public:
 	Partition();
 
-	Partition (std::list<Note> listOfNotes);
+	Partition(std::list<Note> listOfNotes);
 
 	~Partition();
 
@@ -22,16 +24,16 @@ public:
 
 	Partition getPartition(Instrument instrument); // What is that function? What does it should do?
 
-	double getLength (); // Partition length
+	double getLength(); // Partition length
 
-	std::list <double> frameDivision (); // Divide time in frames.
+	std::list <double> frameDivision(); // Divide time in frames.
 
-	// Gives back events (notes) from time startTime to endTime
-	std::list <Note> getPartitionInFrame (double startTime, double endTime);
+										// Gives back events (notes) from time startTime to endTime
+	std::list <Note> getPartitionInFrame(double startTime, double endTime);
 
 	// Gives back the partition from startTime to endTime, completing notes starting or ending
 	// before of after the current frame.
-	std::list <Note> buildPartitionInFrame (double startTime, double endTime);
+	std::list <Note> buildPartitionInFrame(double startTime, double endTime);
 
 };
 
