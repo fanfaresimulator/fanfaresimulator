5fa6e2ab7ba270733c27e1ce3e0beecd1dbf4c54
tbellotti <thomas.bellotti@polytechnique.edu>
2017-11-28 15:17:42 +0100

    partition in frame added

--- include/partition.hpp
+++ include/partition.hpp
@@ -21,6 +21,10 @@ public:
 
 	double getLength (); // Partition length
 
-	std::initializer_list <double> frameDivision (double endTime) // Divide time in frames.
+	std::initializer_list <double> frameDivision (double endTime); // Divide time in frames.
+
+	// Gives back events (notes) from time startTime to endTime
+	std::initializer_list <Note> getPartitionInFrame (double startTime, double endTime);
 
 };
+
