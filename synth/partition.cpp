4b180fbb9d243349627705f315d01c5329d7ecae
Vincent <vincent.mallet96@gmail.com>
2017-11-28 16:44:57 +0100

    clonage des classes de engine

---
+++ synth/partition.cpp
@@ -0,0 +1,5 @@
+//
+// Created by vincent on 28/11/17.
+//
+
+#include "partition.h"
---
+++ synth/partition.h
@@ -0,0 +1,14 @@
+//
+// Created by vincent on 28/11/17.
+//
+
+#ifndef FANFARESIMULATOR_PARTITION_H
+#define FANFARESIMULATOR_PARTITION_H
+
+
+class partition {
+
+};
+
+
+#endif //FANFARESIMULATOR_PARTITION_H
