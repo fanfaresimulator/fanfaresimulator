c3363ae63d22bd4e608c49f54633c7ae39cd348a
Romain Loiseau <romain.loiseau@polytechnique.edu>
2017-11-28 15:58:50 +0100

    string partitions

--- synth/midi_handler.cpp
+++ synth/midi_handler.cpp
@@ -8,6 +8,27 @@
 
 using namespace std;
 
+#define TICK 1            /* time units are MIDI file ticks (absolute)     */
+#define BEAT 2            /* time units are Beats (quarter note, absolute) */
+#define SEC  3            /* time units are seconds (absolute)             */
+#define MSEC 4            /* time units are millisecodns (absolute)        */
+
+
+double getTime(int ticks, MidiFile& midifile, int timetype) {
+   int tpq = midifile.getTicksPerQuarterNote();
+   switch (timetype) {
+      case TICK:
+         return ticks;
+      case BEAT:
+         return (double)ticks/tpq;
+      case SEC:
+         return midifile.getTimeInSeconds(ticks);
+      case MSEC:
+         return 1000 * midifile.getTimeInSeconds(ticks);
+   }
+   return 0.0;
+}
+
 const char *GMinstrument[128] = {
    "acpiano",   "britepno",  "synpiano",  "honkytonk", "epiano1",   "epiano2",
    "hrpschrd",  "clavinet",  "celeste",   "glocken",   "musicbox",  "vibes",
@@ -33,13 +54,49 @@ const char *GMinstrument[128] = {
    "applause",  "ringwhsl"
 };
 
+
+
 void midi_handler() {
   //cout << "Hello world from midi_handler" << endl;
   MidiFile midifile;
   midifile.read("../resources/Movie_Themes_-_Willie_Wonka.mid");
-  double tempo = 120.0;
+
+  Options opts;
+  opts.define("t|ticks|tick=b",                    "display time in ticks");
+  opts.define("s|sec|second|seconds=b",            "display time in seconds");
+  opts.define("m|msec|millisecond|milliseconds=b", "display time in msec");
+  opts.define("b|beat|beats=b",                    "display time in beats");
+
+  char* argvalues[1];
+  std::string s = "../resources/Movie_Themes_-_Willie_Wonka.mid";
+  argvalues[0] = new char[s.length()+1];
+  for (int i = 0; i < s.length(); i++){
+  argvalues[0][i] = s[i];
+  }
+  argvalues[0][s.length()] = '\0';
+  opts.process(1, argvalues);
+
+  int tickQ = opts.getBoolean("ticks");
+  int secQ  = opts.getBoolean("seconds");
+  int msecQ = opts.getBoolean("milliseconds");
+  int beatQ = opts.getBoolean("beats");
+
+  int timetype = SEC;
+
+  if (tickQ) {
+     timetype = TICK;
+  } else if (beatQ) {
+     timetype = BEAT;
+  } else if (secQ) {
+     timetype = SEC;
+  } else if (msecQ) {
+     timetype = MSEC;
+  } else {
+     timetype = BEAT;
+  }
+
   int tracks = midifile.getTrackCount();
-  int TPQ = midifile.getTicksPerQuarterNote();
+  //int TPQ = midifile.getTicksPerQuarterNote();
   // cout << "TPQ: " << TPQ << endl;
   // if (tracks > 1) {
   //     cout << "TRACKS: " << tracks << endl;
@@ -89,7 +146,9 @@ void midi_handler() {
 
             else{
               ostringstream note;
-              note << midifile.getEvent(track, event).tick * 60.0 / (tempo*TPQ) << ";" << instru << ";" << track;
+              //note << midifile.getEvent(track, event).tick * 60.0 / (tempo*TPQ) << ";" << instru << ";" << track;
+              note << getTime(midifile.getEvent(track, event).tick, midifile, timetype)<< ";" << instru << ";" << track;
+
               if(midifile.getEvent(track, event).isNoteOn()){
                 note << ";1";
               }
@@ -99,7 +158,7 @@ void midi_handler() {
               for (int i=1; i<midifile.getEvent(track, event).size(); i++) {  // On commence à 1 car le premier element signifie note on ou noteoff déjà pris en compte
                           note << ";" << (int)midifile.getEvent(track, event)[i];
                 }
-              cout << "NOTE : "<< note.str() << endl;
+              cout << note.str() << endl;
             }
 
 
