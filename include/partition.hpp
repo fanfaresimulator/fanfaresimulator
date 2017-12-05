42eab2a67b4516b403f99f079e93175d35376d13
Vincent <Vincent.ragel@polytechnique.edu>
2017-12-05 18:31:39 +0100

    Creation of  the file Client.cpp One small mistake partition.hpp

--- include/partition.hpp
+++ include/partition.hpp
@@ -10,7 +10,7 @@
 #include "midi_handler.hpp"
 
 #define  MIN_FRAME_LENGTH 0.5 // Minimum frame duration
-#define  MAX_FRAME_LENGTH 0.5 // Maximum frame duration
+#define  MAX_FRAME_LENGTH 3.0 // Maximum frame duration
 
 class Partition {
 private:
