a76e6f6b10e7dcd56bb21ce032c50f399318e799
tbellotti <thomas.bellotti@polytechnique.edu>
2017-12-05 15:58:04 +0100

    Modif

--- include/noteglobale.hpp
+++ include/noteglobale.hpp
@@ -2,7 +2,8 @@
 
 #include <string>
 #include <note>
-#include "instrument.hpp"
+//#include "instrument.hpp"
+#include "pupitre.hpp"
 
 class NoteGlobale {
 private:
@@ -10,12 +11,12 @@ private:
 	int key;			// from 0 to 3
 	double timestamp;
 	bool signal;
-	Instrument instrument;
+	Pupitre pupitre;
 
 public:
 	NoteGlobale();
 
-	NoteGlobale(std::list <Note> listOfNotes, int key, double timestamp, bool signal);
+	NoteGlobale(std::list <Note> listOfNotes, int key, double timestamp, bool signal, Pupitre pupitrenew);
 
 	~NoteGlobale();
 	
