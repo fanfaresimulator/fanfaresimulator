c89735a1fbc88f5f5e54601ffe1d41b6b0748166
tbellotti <thomas.bellotti@polytechnique.edu>
2017-12-05 15:58:09 +0100

    Modif

--- include/note.hpp
+++ include/note.hpp
@@ -1,19 +1,20 @@
 #pragma once
 
 #include <string>
-#include "instrument.hpp"
+//#include "instrument.hpp"
+#include "pupitre.hpp"
 
 class Note {
 private:
     double timestamp;
     bool signal;                // on/off
-    Instrument instrument;      // channel
+    Pupitre pupitre;      // channel
     int key;                    // note 0 to 127
     int velocity;               // value 0 to 127
-    int track;					// value 0 to #track
+    //int track;					// value 0 to #track
 
 public:
-    Note(double timestamp, bool signal, Instrument instrument, int key, int velocity, int track);
+    Note(double timestamp, bool signal, Pupitre pupitredeux, int key, int velocity, int track);
 
     ~Note();
 
@@ -21,13 +22,13 @@ public:
 
     bool getSignal();
 
-    Instrument getInstrument();
+    Pupitre getPupitre();
 
     int getKey();
 
     int getVelocity();
 
-    int getTrack();
+   // int getTrack();
 
     bool isEqual(Note note2); // Compare timestamp and key.
 
