5cbc93b8d71c4a36cf66e3b1360de8828536a8c1
tbellotti <thomas.bellotti@polytechnique.edu>
2017-12-06 11:28:54 +0100

    added partitionglobale

---
+++ synth/partitionglobale.cpp
@@ -0,0 +1,40 @@
+// Created by Thomas Bellotti on 02/12/2017
+
+
+#include "../include/partitionglobale.hpp"
+#include "../include/partition.hpp"
+
+
+
+
+PartitionGlobale::PartitionGlobale ()	{
+
+}
+
+PartitionGlobale::PartitionGlobale (Partition mypartition)	{
+
+	std::vector <NoteGlobale> finalListOfNotes;
+	std::vector <double> frames = mypartition.frameDivision();
+
+	std::vector <Note> noteSet;
+
+
+	for (std::vector<double>::iterator temps=frames.begin(); temps != (frames.end()-1); temps++)	{
+
+		double startTime = (*temps);
+		double endTime = (*(temps+1));
+
+		std::vector <Note> actualSegment = mypartition.buildPartitionInFrame(startTime,endTime,noteSet);
+
+		int randomKey = rand()%3 + 1;
+		finalListOfNotes.push_back(NoteGlobale(actualSegment, randomKey,startTime, true, actualSegment.begin()->getPupitre()));
+		finalListOfNotes.push_back(NoteGlobale(actualSegment, randomKey,endTime,false,actualSegment.begin()->getPupitre()));
+
+	}
+
+}
+
+PartitionGlobale::~PartitionGlobale()	{
+	//delete listOfNotes;
+}
+
