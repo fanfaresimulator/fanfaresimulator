892c864937ababbaa8855f5b8260b67305d28702
emersion <contact@emersion.fr>
2017-12-06 22:20:38 +0100

    Remove useless include in server

--- server.cpp
+++ server.cpp
@@ -1,17 +1,9 @@
+#include <QApplication>
 #include "include/network/server.hpp"
 #include "include/network/advertizer.hpp"
 #include "include/server.hpp"
 #include "include/sound_player.hpp"
 #include "include/midi_handler.hpp"
-
-#include <QApplication>
-
-#ifdef __unix__
-#include <unistd.h>
-#elif defined(_WIN32) || defined(WIN32)
-#include <windows.h>
-#endif
-
 
 int main(int argc, char *argv[]) {
 	QApplication app(argc, argv);
