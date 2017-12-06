00fc50fa9e4e6953b23bcb9df84b9a59abf0e513
tbellotti <thomas.bellotti@polytechnique.edu>
2017-12-06 18:01:34 +0100

    Changé nom fonction

--- include/partitionglobale.hpp
+++ include/partitionglobale.hpp
@@ -25,7 +25,7 @@ public:
 	Partition getPartition(Pupitre pupitre);
 
 	// Cette fonction peut donner listOfNotes.end() lorsqu'on est a la fin de la partiture ... il faut le gerer.
-	std::vector<NoteGlobale>::iterator f1(std::vector<NoteGlobale>::iterator iterActual, double actualTime);
+	std::vector<NoteGlobale>::iterator getNextValidIterator(std::vector<NoteGlobale>::iterator iterActual, double actualTime);
 
 
 
