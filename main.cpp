3779f9bbc7ed99c1edb76ae5f6ed808cd07359fc
emersion <contact@emersion.fr>
2017-12-10 18:36:11 +0100

    Add CLI options

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <QApplication>
+#include <QCommandLineParser>
 #include "include/client.hpp"
 #include "include/UsernameWindow.hpp"
+#include "include/network/network.hpp"
 
 int main(int argc, char *argv[]) {
 	QApplication app(argc, argv);
+	app.setApplicationName("fanfaresimulator-client");
+	app.setApplicationVersion("1.0");
+
+	QCommandLineParser parser;
+	parser.setApplicationDescription("FanfareSimulator 2k client");
+	parser.addHelpOption();
+	parser.addVersionOption();
+	QCommandLineOption serverHostOption("server-host", "Server host", "host");
+	parser.addOption(serverHostOption);
+	parser.process(app);
 
 	// username screen
 	UsernameWindow usernameWindow;
@@ -14,6 +26,11 @@ int main(int argc, char *argv[]) {
 
 	// client engine create
 	Client engine(&app, username);
+
+	if (parser.isSet(serverHostOption)) {
+		QHostAddress serverHost = QHostAddress(parser.value(serverHostOption));
+		engine.connectToServer(serverHost, PORT_NO);
+	}
 
 	return app.exec();
 }
